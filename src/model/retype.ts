8908a81c21649ceaaa7cee5d8869e03aa01b98e9
FunderJM <jer.fun1@gmail.com>
2019-04-12 15:57:19 -0400

    Added retype class

---
+++ src/model/retype.ts
@@ -0,0 +1,9 @@
+import { reimbursementtype } from "./reimbursementtype";
+
+
+export let retype : reimbursementtype[] = [
+    new reimbursementtype(1, 'Lodging'),
+    new reimbursementtype(2, 'Travel'),
+    new reimbursementtype(3, 'Food'),
+    new reimbursementtype(4, 'Other')
+]
