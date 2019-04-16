53d78354f773ca944f17996edec7dff37d34d2b7
FunderJM <jer.fun1@gmail.com>
2019-04-15 23:30:01 -0400

    Added function to reimbursement router

--- src/daos/reimbursement.dao.ts
+++ src/daos/reimbursement.dao.ts
@@ -12,5 +12,28 @@ export async function findAllRequestByStatusId(status: number) {
         const result = await client.query(queryString, [status]);
         const restat = result.rows[0];
         return restat;
+    } catch (err) {
+        console.log(err);
+        return undefined;
+    } finally {
+        client && client.release();
+    }
+}
+
+export async function findAllRequestByUserId(userId: number) {
+    let client: PoolClient;
+    try {
+        client = await connectionPool.connect();
+        const queryString = `SELECT * FROM project0.reimbursement as re
+        INNER JOIN project0.employee as emp ON (re.author = emp.userid)
+        WHERE re.author = $1 ORDER BY re.datesubmitted ASC;`;
+        const result = await client.query(queryString, [userId]);
+        const reuser = result.rows[0];
+        return reuser;
+    } catch (err) {
+        console.log(err);
+        return undefined;
+    } finally {
+        client && client.release();
     }
 }
